6c5ca408399e93108a11b8364eeafe32b2c1bd6b
Emily Chooi <emilychooi.k@northeastern.edu>
2025-11-03 23:06:27 -0500

    a4

--- app/Labs/Lab4/ReduxExamples/page.tsx
+++ app/Labs/Lab4/ReduxExamples/page.tsx
@@ -1,17 +1,6 @@
 "use client"
-import HelloRedux from "./HelloRedux";
-import CounterRedux from "./CounterRedux";
-import AddRedux from "./AddRedux";
-import TodoList from "./todos/TodoList";
+import ReduxExamples from "./index";
 
-export default function ReduxExamples() {
-  return (
-    <div>
-      <h2>Redux Examples</h2>
-      <HelloRedux />
-      <CounterRedux />
-      <AddRedux />
-      <TodoList />
-    </div>
-  );
+export default function ReduxExamplesPage() {
+  return <ReduxExamples />;
 }
